f5eb53a27caa23892b65ca6fc655a3fd49e58b31
Abdulbasit Mamman <basttyy@gmail.com>
2025-10-16 10:44:06 +0100

    initialize klinecharts

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+import { KLineChartPro, DefaultDatafeed } from '@klinecharts/pro'
+import '@klinecharts/pro/dist/klinecharts-pro.css'
+
+onMounted(() => {
+  const chart = new KLineChartPro({
+    container: document.getElementById('container') || '',
+    // Default symbol info
+    symbol: {
+      exchange: 'XNYS',
+      market: 'stocks',
+      name: 'Alibaba Group Holding Limited American Depositary Shares, each represents eight Ordinary Shares',
+      shortName: 'BABA',
+      ticker: 'BABA',
+      priceCurrency: 'usd',
+      type: 'ADRC',
+    },
+    // Default period
+    period: { multiplier: 15, timespan: 'minute', text: '15m' },
+    // The default data access is used here. If the default data is also used in actual use, you need to go to the https://polygon.io/ apply for API key
+    datafeed: new DefaultDatafeed(`${import.meta.env.VITE_POLYGON_API_KEY}`),
+  })
+  window.addEventListener('resize', () => {
+    //TODO: Resize chart
+  })
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div id="container"></div>
 </template>
 
 <style scoped>
