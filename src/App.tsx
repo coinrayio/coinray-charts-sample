7201a95f64e43babd46a48b779dec80e03cb190a
Abdulbasit Mamman <basttyy@gmail.com>
2025-10-23 13:44:25 +0100

    storing temporary progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react'
-import { KLineChartPro } from '@klinecharts/pro'
+import { DummyOrderController, KLineChartPro } from '@klinecharts/pro'
 import '@klinecharts/pro/dist/klinecharts-pro.css'
 import './App.css'
 import { CoinrayDatafeed } from './CoinrayDatafeed'
 
 function App() {
   let initialized = false;
+  let datafeed = new CoinrayDatafeed();
 
   useEffect(() => {
     if ((import.meta.env.VITE_APP_ENV || 'development') as string === 'development') {
@@ -26,12 +27,32 @@ function App() {
         priceCurrency: 'usd',
         type: 'ADRC',
       },
-      period: { multiplier: 1, timespan: 'minute', text: '1m' },
-      datafeed: new CoinrayDatafeed(),
+      period: { span: 15, type: 'minute', text: '15m' },
+      locale: 'en-US',
+      theme: 'light',
+      mainIndicators: [],
+      subIndicators: [],
+      datafeed,
+      orderController: new DummyOrderController()
     })
 
     const handleResize = () => chart.resize()
     window.addEventListener('resize', handleResize)
+
+    setTimeout(() => {
+      const line = chart.getInstanceApi()?.createOverlay({ name: 'positionLine', paneId: 'candle_pane', points: [{ timestamp: datafeed.firstData[datafeed.firstData.length - 40].timestamp, value: datafeed.firstData[datafeed.firstData.length - 40].close}]}) as string
+      console.info("Created order line:", line)
+      const orderLine = chart.getInstanceApi()?.getOverlays({ name: 'positionLine', id: line, paneId: 'candle_pane'}).at(0) || null
+      if (orderLine && datafeed.firstData.length > 5) {
+        setTimeout(() => {
+          orderLine.setPrice(datafeed.firstData[datafeed.firstData.length - 5].close)
+            .setText('Buystop Line')
+        }, 10000)
+      } else {
+        console.log("No order line set")
+      }
+    }, 8000)
+
     return () => {
       window.removeEventListener('resize', handleResize)
       // chart?.dispose()
--- src/CoinrayDatafeed.ts
+++ src/CoinrayDatafeed.ts
@@ -1,7 +1,7 @@
 import type { Datafeed, DatafeedSubscribeCallback, Period, SymbolInfo } from "@klinecharts/pro";
 import Coinray, { CoinrayCache } from "coinrayjs";
 import type { Candle } from "coinrayjs/dist/types";
-import type { KLineData } from "klinecharts";
+import type { KLineData, Nullable, Overlay } from "klinecharts";
 import type { SubscribeCandlesData } from "./types/CoinrayFeedTypes";
 
 export class CoinrayDatafeed implements Datafeed
@@ -11,6 +11,7 @@ export class CoinrayDatafeed implements Datafeed
     private firstLoad: boolean;
     // private lastBarTime: number;
     private lastCandle: Candle | null;
+    public firstData: KLineData[] = [];
 
     constructor() {
         let token = (import.meta.env.VITE_COINRAY_API_KEY || '') as string
@@ -39,14 +40,14 @@ export class CoinrayDatafeed implements Datafeed
         from /= 1000;
         to /= 1000;
         if (!this.firstLoad) {
-            to = to + (period.multiplier * this.timespanToSeconds(period.timespan));
+            to = to + (period.span * this.timespanToMinutes(period.type) * 60);
         }
-        console.info({ symbol, period, from, to })
-        const candles = await this.api.fetchCandles({ coinraySymbol: `${symbol.exchange}_${symbol.ticker ?? symbol.shortName}`, resolution: `${period.multiplier * this.timespanToSeconds(period.timespan)}`, start: from, end: to, useWebSocket: false });
-        console.info(candles)
+        console.info('getHistoryKLineData called with:' ,{ symbol, period, from, to })
+        const candles = await this.api.fetchCandles({ coinraySymbol: `${symbol.exchange}_${symbol.ticker ?? symbol.shortName}`, resolution: `${period.span * this.timespanToMinutes(period.type)}`, start: from, end: to, useWebSocket: false });
+        // console.info(candles)
 
-        if (!this.firstLoad && candles.length > 0) {
-            this.lastCandle = candles.pop() || null;
+        if (this.firstLoad && candles.length > 0) {
+            this.lastCandle = candles[candles.length - 1] || null;    
         }
 
         const data: KLineData[] = candles.map(c => ({
@@ -56,12 +57,15 @@ export class CoinrayDatafeed implements Datafeed
         }));
 
         console.info(data);
+        if (this.firstLoad) {
+            this.firstData = data;
+        }
         this.firstLoad = false;
         return data;
     }
 
     subscribe(symbol: SymbolInfo, period: Period, callback: DatafeedSubscribeCallback): void {
-        this.api.subscribeCandles({ coinraySymbol: `${symbol.exchange}_${symbol.ticker ?? symbol.shortName}`, resolution: `${period.multiplier * this.timespanToSeconds(period.timespan)}`, lastCandle: this.lastCandle || undefined }, (data: SubscribeCandlesData) => {
+        this.api.subscribeCandles({ coinraySymbol: `${symbol.exchange}_${symbol.ticker ?? symbol.shortName}`, resolution: `${period.span * this.timespanToMinutes(period.type)}`, lastCandle: this.lastCandle || undefined }, (data: SubscribeCandlesData) => {
             const candle = {
                 ...data.candle,
                 volume: data.candle.baseVolume,
@@ -72,10 +76,10 @@ export class CoinrayDatafeed implements Datafeed
     }
 
     unsubscribe(symbol: SymbolInfo, period: Period): void {
-        this.api.unsubscribeCandles({ coinraySymbol: `${symbol.exchange}_${symbol.ticker ?? symbol.shortName}`, resolution: `${period.multiplier * this.timespanToSeconds(period.timespan)}` });
+        this.api.unsubscribeCandles({ coinraySymbol: `${symbol.exchange}_${symbol.ticker ?? symbol.shortName}`, resolution: `${period.span * this.timespanToMinutes(period.type)}` });
     }
 
-    private timespanToSeconds(timespan: string): number {
+    private timespanToMinutes(timespan: string): number {
         switch (timespan) {
             case 'minute': return 1;
             case 'hour': return 60;
